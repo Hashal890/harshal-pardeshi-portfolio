519b272bf781bbff9a0f3bf555f518a9179394b8
amolpradeshi <pardeshiharshal90@gmail.com>
2024-06-28 02:34:55 +0530

    created about me section

---
+++ src/components/about/AboutMeCard.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import ProfilePhoto from "../../assets/Harshal-Pardeshi-Profile-Photo.png";
+
+const AboutMeCard = () => {
+  return (
+    <Box>
+      <Heading
+        textAlign={"center"}
+        bgGradient="linear(to-r, teal.400, green.500)"
+        bgClip="text"
+        fontWeight="extrabold"
+        fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}
+        my={8}
+      >
+        About Me
+      </Heading>
+      <Flex
+        justifyContent={"center"}
+        alignItems={"center"}
+        gap={4}
+        flexDir={["column-reverse", "column-reverse", "row"]}
+      >
+        <Box>
+          <Text color={"gray"} mb={4}>
+            As a Full Stack Web Developer, I have accumulated a year of
+            practical experience in full-stack web development. My journey began
+            with 7 months of rigorous training at Masai School, where I
+            developed my proficiency in both frontend and backend technologies.
+            Following this, I dedicated 5 months to working on personal
+            projects, which helped me further refine my web development skills.
+            Currently, I am pursuing a B.Sc. in Computer Science from BITS
+            Pilani and am willing to relocate for the right opportunity.
+          </Text>
+          <Text color={"gray"}>
+            I am passionate about using technology to solve real-world problems
+            and am driven by a genuine love for coding and a dedication to
+            continuous learning. I find great satisfaction in tackling complex
+            challenges and thrive in environments that encourage innovation and
+            growth. My goal is to contribute to impactful projects that make a
+            significant difference. Whether working independently or as part of
+            a team, I am committed to delivering high-quality, innovative
+            solutions that can transform lives.
+          </Text>
+        </Box>
+        <Image
+          src={ProfilePhoto}
+          alt={"profile-photo"}
+          w={"300px"}
+          h={"300px"}
+          borderRadius={"30px"}
+          border={"3px dotted cyan"}
+        />
+      </Flex>
+    </Box>
+  );
+};
+
+export default AboutMeCard;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
+import AboutMeCard from "../components/about/AboutMeCard";
 
 const About = () => {
   return (
-    <Box id={"about"}>
-      About
+    <Box id={"about"} w={["95%", "95%", "80%"]} m={"auto"} pt={4}>
+      <AboutMeCard />
     </Box>
   );
 };
