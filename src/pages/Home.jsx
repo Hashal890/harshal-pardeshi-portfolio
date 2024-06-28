5dbdb02f0f46991769b4bedd8d87f49549d4a636
amolpradeshi <pardeshiharshal90@gmail.com>
2024-06-28 13:42:10 +0530

    changed home section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,12 +21,11 @@ export default function Home() {
         A Full Stack Web Developer
       </Code>
       <Text maxW={"600px"} textAlign={"center"} color={"gray"}>
-        Aspiring Full Stack Web Developer with a strong foundation in MERN
-        stacks, Next.js, Redux, Node.js, JavaScript, TypeScript, HTML, CSS and
-        DSA. Dedicated to delivering high-quality web applications and
-        solutions. Proven ability to collaborate effectively in team
-        environments, with a keen eye for detail and a passion for continuous
-        learning and growth.
+        with a strong foundation in MERN stacks, Next.js, Redux, Node.js,
+        JavaScript, TypeScript, HTML, CSS and DSA. Dedicated to delivering
+        high-quality web applications and solutions. Proven ability to
+        collaborate effectively in team environments, with a keen eye for detail
+        and a passion for continuous learning and growth.
       </Text>
       <ResumeButtons />
     </Flex>
